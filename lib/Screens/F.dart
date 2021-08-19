254a8886c94f3625586cfb0e16d275d629c69590
unknown <softech.alayth234@gmail.com>
2021-08-19 19:09:11 +0300

    we changed again

--- lib/Screens/F.dart
+++ lib/Screens/F.dart
@@ -15,7 +15,7 @@ class _forvirateState extends State<forvirate> {
       height: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text("صفحة تخيل ")],
+        children: [Text("صفحة عبدالشكور ")],
       ),
     );
   }
