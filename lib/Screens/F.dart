6d06cb8a3ca9b689602e2a9e24ff48d014174a67
unknown <softech.alayth234@gmail.com>
2021-08-19 17:21:11 +0300

    i changed the name of page favotite

--- lib/Screens/F.dart
+++ lib/Screens/F.dart
@@ -15,7 +15,7 @@ class _forvirateState extends State<forvirate> {
       height: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text("صفحة المفضله")],
+        children: [Text("صفحة الخشن")],
       ),
     );
   }
